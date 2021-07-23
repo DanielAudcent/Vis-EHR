8aec1fe79b8992cc3ae4a53d4f8f563c174a3e57
Daniel Audcent <danielaudcent@gmail.com>
2021-07-23 18:50:27 +0100

    js visualisation initial setup

---
+++ JS Vis/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- html code goes here -->
+  <div id="app">
+    <Shapefile>
+    </Shapefile>
+  </div>
+</template>
+
+<script>
+// js code goes here
+import Shapefile from './components/Shapefile.vue'
+ 
+export default {
+  name: 'App',
+  components: {
+    Shapefile
+  }
+}
+</script>
+
+<style>
+/* css code goes here */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ JS Vis/src/components/Shapefile.vue
@@ -0,0 +1,40 @@
+<template>
+<!-- html code goes here -->
+  <div class="test">
+      this is a test
+
+  </div>
+</template>
+
+<script>
+// insert js code here
+import * as d3 from 'd3'
+import * as topo from 'topojson-client'
+
+
+export default {
+  name: 'Shapefile',
+  methods: {
+    // init
+
+    // funcs
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
